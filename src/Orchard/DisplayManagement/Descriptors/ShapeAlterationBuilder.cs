b61df799f45159ecfc063629d3dcf08f17d918fa
Tom Pratt <tom.pratt@live.com>
2016-09-08 14:20:06 +0200

    Placement parsing strategy match BindingType

--- src/Orchard/DisplayManagement/Descriptors/ShapeAlterationBuilder.cs
+++ src/Orchard/DisplayManagement/Descriptors/ShapeAlterationBuilder.cs
@@ -124,6 +124,7 @@ namespace Orchard.DisplayManagement.Descriptors {
         public string ContentType { get; set; }
         public string Stereotype { get; set; }
         public string DisplayType { get; set; }
+        public string BindingType { get; set; }
         public string Differentiator { get; set; }
         public string Path { get; set; }
         public string Source { get; set; }
